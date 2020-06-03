57fe410347c3df2c2b800ef8be27177bb08ee7d6
rozsay <rozsay@gmail.com>
2020-06-03 06:13:43 +0000

    1.5

--- install.sh
+++ install.sh
@@ -14,6 +14,6 @@ sudo apt-get install software-properties-common ansible git python-apt -y
 #####################################
 # Display real installation process #
 echo ""
-echo "Customize the playbook ansible-desktop.yml to suit your needs, then run ansible with :"
-echo "  ansible-playbook ansible-desktop.yml --ask-become-pass"
+echo "Customize the playbook playbook.yml to suit your needs, then run ansible with :"
+echo "  ansible-playbook playbook.yml --ask-become-pass"
 echo ""
